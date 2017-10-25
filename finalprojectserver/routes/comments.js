75944fa831e294372666f410e36bc848043ea80e
Elena_Wei <weiyaorui@gmail.com>
2017-10-25 10:56:28 -0500

    Add post and comments for DB

---
+++ finalprojectserver/routes/comments.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/finalproject', {useMongoClient:true});
+
+const commentSchema = new mongoose.Schema({
+    username: String,
+    post_id: String,
+    content: String,
+    like: [String]
+});
+const Comment = mongoose.model('comment', commentSchema);
+
+router.post('/create', (req, res)=>{
+    console.log(req.body);
+    const comment = new Comment(req.body);
+    comment.save((err, comment)=>{
+        if(err) return console.error(err);
+        console.log('Comment successfully!')
+        res.json(comment._id);
+    })
+})
+
+router.get('/', (req, res) => {
+    Comment.find((err, comments)=>{
+        if(err) return console.error(err);
+        res.json(comments);
+    })
+})
+
+router.put('/update/:id', (req, res)=>{
+    Comment.findOneAndUpdate({_id: req.params.id}, req.body, function(err, comment){
+        if(err) return console.error(err);
+        console.log('Comment is updated successfully!')
+    });
+    res.json('ok');
+})
+
+router.delete('/delete/:id', (req, res)=>{
+    Comment.remove({_id: req.params.id}, (err)=>{
+        if(err) return console.error(err);
+        console.log('Comment is removed successfully!')
+        res.json('ok');
+    })
+})
+
+module.exports = router;
---
+++ finalprojectserver/routes/posts.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/finalproject', {useMongoClient:true});
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    type: String,
+    author: String,
+    rating: {
+        username: String,
+        rating: Number
+    }
+});
+const Post = mongoose.model('post', postSchema);
+
+router.post('/create', (req, res)=>{
+    console.log(req.body);
+    const post = new Post(req.body);
+    post.save((err, post)=>{
+        if(err) return console.error(err);
+        console.log(post.title + " is successfully created!")
+        res.json(post._id);
+    })
+})
+
+router.get('/', (req, res) => {
+    Post.find((err, posts)=>{
+        if(err) return console.error(err);
+        res.json(posts);
+    })
+})
+
+router.put('/update/:id', (req, res)=>{
+    Post.findOneAndUpdate({_id: req.params.id}, req.body, function(err, post){
+        if(err) return console.error(err);
+        console.log('Post:' + post.title + 'is updated successfully!')
+    });
+    res.json('ok');
+})
+
+router.delete('/delete/:id', (req, res)=>{
+    Post.remove({_id: req.params.id}, (err, post)=>{
+        if(err) return console.error(err);
+        console.log('Post:' + post.title + ' is successfully removed!')
+        res.json('ok');
+    })
+})
+  
+module.exports = router;
