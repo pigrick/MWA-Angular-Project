9521d9d7b4ac584041f6283ae4a932747e41ef3e
Elena_Wei <weiyaorui@gmail.com>
2017-10-25 22:54:28 -0500

    update app component

--- src/app/component/app.component.ts
+++ src/app/component/app.component.ts
@@ -6,20 +6,6 @@ import { AuthService } from "../service/auth.service";
   templateUrl: './app.html',
   styles: []
 })
-<<<<<<< HEAD
-export class AppComponent implements DoCheck {
-  title = 'Welcome to Our Website';
-  loggedin;
-
-  constructor(private authService:AuthService){}
-
-  ngDoCheck(){
-    if(this.authService.loggedIn()){
-      this.loggedin = true;
-    } else {
-      this.loggedin = false;
-    }
-=======
 export class AppComponent {
   title = 'app';
 
@@ -28,8 +14,7 @@ export class AppComponent {
 
   getLocalUser(){
     return localStorage.getItem('username');
->>>>>>> d3fa164c5d9048275d4af19e115d26571806ec94
-  }
+}
   getLocalAuthorization(){
     return localStorage.getItem('authorization');
   }
